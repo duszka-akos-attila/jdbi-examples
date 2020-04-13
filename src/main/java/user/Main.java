2888023dc05a4f50e0937ec2920c96cd0394df01
Duszka Ákos Attila <duszka.akos.attila@gmail.com>
2020-04-13 12:18:49 +0200

    Updating jdbi-example forked project with user package.

---
+++ src/main/java/user/Main.java
@@ -0,0 +1,45 @@
+package user;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+import static user.User.Gender.MALE;
+import static user.User.Gender.OTHER;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        try (Handle handle = jdbi.open()) {
+
+            UserDao dao = handle.attach(UserDao.class);
+            dao.createTable();
+
+            User usr01 = User.builder()
+                    .id(1L)
+                    .username("bödi")
+                    .password("letmein")
+                    .name("Zzíros B. Ödön")
+                    .email("bödön_man@citromail.hu")
+                    .gender(OTHER)
+                    .dob(LocalDate.parse("2020-04-13"))
+                    .enabled(true)
+                    .build();
+
+            dao.insert(usr01);
+
+            System.out.println(dao.findById(usr01.getId()) + " was found by ID.");
+            System.out.println(dao.findByUsername(usr01.getUsername()) +" was found by Username.");
+
+            dao.list().stream().forEach(System.out::println);
+
+            dao.delete(usr01);
+        }
+    }
+}
